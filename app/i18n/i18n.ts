9ec6bee5bbf190e5334e542ae1471560b172f2f3
Denish Makadiya <denish.makadiya@tatvasoft.com>
2021-05-07 19:47:16 +0530

    Removed unimodules for expo

--- app/i18n/i18n.ts
+++ app/i18n/i18n.ts
@@ -1,22 +1,14 @@
-import * as Localization from "expo-localization"
+import * as RNLocalize from "react-native-localize"
 import i18n from "i18n-js"
-import en from "./en.json"
-import ja from "./ja.json"
+
+const en = require("./en")
+const ja = require("./ja")
 
 i18n.fallbacks = true
 i18n.translations = { en, ja }
 
-i18n.locale = Localization.locale || "en"
+const fallback = { languageTag: "en", isRTL: false }
 
-/**
- * Builds up valid keypaths for translations.
- * Update to your default locale of choice if not English.
- */
-type DefaultLocale = typeof en
-export type TxKeyPath = RecursiveKeyOf<DefaultLocale>
-
-type RecursiveKeyOf<TObj extends Record<string, any>> = {
-  [TKey in keyof TObj & string]: TObj[TKey] extends Record<string, any>
-    ? `${TKey}` | `${TKey}.${RecursiveKeyOf<TObj[TKey]>}`
-    : `${TKey}`
-}[keyof TObj & string]
+const { languageTag } =
+  RNLocalize.findBestAvailableLanguage(Object.keys(i18n.translations)) || fallback
+i18n.locale = languageTag
